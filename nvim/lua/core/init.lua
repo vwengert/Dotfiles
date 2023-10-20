c23b3d7ed7c600037d0dbb228460bfd40d700140
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2023-10-20 20:01:35 +0200

    leave insert mode with fast jk

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -8,6 +8,7 @@ g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 g.toggle_theme_icon = "   "
 g.transparency = config.ui.transparency
 vim.wo.relativenumber = true
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
 
 -------------------------------------- options ------------------------------------------
 opt.laststatus = 3 -- global statusline
