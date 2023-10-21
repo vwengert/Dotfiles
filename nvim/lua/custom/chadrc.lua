6226dbc8c31be1379921e167e6ad6ce9f9ecbe4e
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2023-10-21 08:16:40 +0200

    sets up a different statusline theme and base theme

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,6 +1,16 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'catppuccin' }
 M.plugins = "custom.plugins"
+M.ui = {
+  theme = 'gruvbox',
+  statusline = {
+    theme = "vscode_colored",
+    override = {
+      mode = function()
+        return ""
+      end,
+    },
+  },
+}
 M.mappings = require("custom.mappings")
 return M
