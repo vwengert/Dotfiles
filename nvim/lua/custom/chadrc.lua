cd91ced4da419e108ea8eba4b01ed80396ab6880
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2023-10-23 16:57:59 +0200

    remove transparency and not used statusline code

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -3,15 +3,6 @@ local M = {}
 M.plugins = "custom.plugins"
 M.ui = {
   theme = 'catppuccin',
-  transparency = true,
-  statusline = {
-    theme = "default",
-    override = {
-      mode = function()
-        return ""
-      end,
-    },
-  },
 }
 M.mappings = require("custom.mappings")
 return M
