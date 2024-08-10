f65801012865478a6dab9a8cfc41df30486bc769
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-08-10 08:36:38 +0200

    adding delta to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-export PATH="$PATH:/opt/nvim-linux64/bin:/home/volker/.local/bin"
+export PATH="$PATH:/opt/nvim-linux64/bin:/home/volker/.local/bin:/opt/delta-0.17.0-i686-unknown-linux-gnu"
 alias vim=nvim
