02c19a251f31faa8da9489f2dafc352e6566bfcc
Volker <22374206+vwengert@users.noreply.github.com>
2024-09-16 08:18:26 +0200

    wh add alias to open winterhalter project in vim

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,4 @@ export PATH="$PATH:/opt/nvim-linux64/bin:/home/volker/.local/bin"
 alias vim=nvim
 alias agdb='cgdb -d /opt/winterhalter-distro/4.3/sysroots/x86_64-wihasdk-linux/usr/bin/arm-wiha-linux-gnueabi/arm-wiha-linux-gnueabi-gdb'
 alias hc='cd ~/develop/winterhalter-hmi/'
+alias hv='cd ~/develop/winterhalter-hmi/ && vim'
