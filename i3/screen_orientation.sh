02adfe8f70c67e05a5a46c813512e5759637a023
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2025-03-27 16:52:44 +0100

    i3 configuration fixed for 3 monitor layout

---
+++ i3/screen_orientation.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 4520x840 --rotate normal --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate right --rate 59.94 --output DP-2-3 --mode 3440x1440 --pos 1080x480 --rotate normal
+xrandr --output eDP-1 --mode 1920x1080 --pos 4520x840 --rotate normal --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate right --rate 59.94 --output DP-1-3 --mode 3440x1440 --pos 1080x480 --rotate normal
