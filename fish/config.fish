6d311d3c0b13b7178c9a22855d6e00569d27b9c4
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2025-03-29 12:44:23 +0100

    adding fzf alias and remove wrong Path values

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-export PATH="$PATH:/opt/nvim-linux64/bin:/home/volker/.local/bin:/opt/delta-0.17.0-i686-unknown-linux-gnu"
 alias vim=nvim
+alias fzf="fzf --style full --preview 'bat {}' --bind 'focus:transform-header:file --brief {}'"
