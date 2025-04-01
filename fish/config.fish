3bd1a93b358784d44de161586bf8431be8738d86
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2025-04-01 10:16:36 +0200

    new fish config in arch

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+fzf --fish | source
 alias vim=nvim
-alias fzf="fzf --style full --preview 'bat {}' --bind 'focus:transform-header:file --brief {}'"
+alias fzf="fzf --style full --preview 'bat --color=always {}' --bind 'focus:transform-header:file --brief {}'"
+alias mux="tmux new -A -s dev -n code -c /home/volker/Repos/"
